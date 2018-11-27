fa453fedd0cb40f47598eb95146f258579de43d8
MChen <mfchen@outlook.com>
2018-11-26 23:11:25 -0500

    Add BufPair2FileHashitem as a two level hash table to store <src buf, dst buf> to file.

--- dataToFile.h
+++ dataToFile.h
@@ -6,16 +6,42 @@
 #ifndef DATATOFILE_H_
 #define DATATOFILE_H_
 
+#include <stdio.h>  // FILE
 #include "bufHitCountArray.h"
 
+/*
+ * Data to file context:
+ *  contains context info to write <src,dst> propagation pair
+ *  into files.
+ */
 typedef struct Data2FileCtxt_{
   BufHitCountAryCtxt *bufHitCntAryCtxt;
 } Data2FileCtxt;
+
+/*
+ * Two level hash table to file ptr
+ *  1) src buf ID as 1st level hash
+ *  2) dst buf ID as 2nd level hash
+ *  <src buf ID, dst buf ID>: File *fl ptr
+ */
+typedef struct BufPair2FileHashItem_
+{
+  u32 bufID; // Used as key to hash: src buf ID in 1st level; dst buf ID in 2nd level
+  struct BufPair2FileHashItem_ *subHash; // next level hash
+  FILE *fl; // the file ptr the <src buf ID, dst buf ID> points to
+  UT_hash_handle hh_bufPair2File;  // makes this structure hashable
+} BufPair2FileHashItem;
 
 Data2FileCtxt *
 newData2FileCtxt(BufHitCountAryCtxt *bufHitCntAryCtxt);
 
 void
 delData2FileCtxt(Data2FileCtxt *data2FlCtxt);
+
+BufPair2FileHashItem *
+newBufPair2FileHashItem(u32 bufID, BufPair2FileHashItem *subHash, FILE *fl);
+
+void
+delBufPair2FileHashItem(BufPair2FileHashItem *bufPair2FileHashItem);
 
 #endif /* DATATOFILE_H_ */
